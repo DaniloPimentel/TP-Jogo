f5cecd08206776df381cbf6df16fed3c3f35d2cb
Danilo Pimentel <danilo.pimentel97@gmail.com>
2015-10-13 07:55:06 -0300

    Alteração em Cliente
    Interface adicionada

--- Cliente/src/cliente/Cliente.java
+++ Cliente/src/cliente/Cliente.java
@@ -1,12 +1,39 @@
 package cliente;
 
+import form.Janela;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+
 public class Cliente {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        new Cliente();
+        
     }
     
+    public static final int SERVICO_OLA = 1;
+    public static final int SERVICO_MUDAR_APELIDO = 2;
+    public static final int SERVICO_SOLICITAR_IDS = 3;
+    public static final int SERVICO_SOLICITAR_APELIDO = 4;
+    public static final int SERVICO_MENSAGEM = 5;
+    
+    public static final String ERRO_CONEXAO = "Erro ao se conectar ao servidor";
+    public static final String ERRO_SERVICO_OLA = "Erro ao enviar o apelido ao servidor. Tente novamente.";
+    
+    public Socket socket;
+    Janela janela;
+    
+    Cliente(){
+        janela = new Janela(this);
+    }
+    
+    public void enviar(Requisicao requisicao) throws IOException {
+
+        PrintStream ps = new PrintStream(this.socket.getOutputStream());
+        ps.println(requisicao.encode());
+
+    }
+
 }
---
+++ Cliente/src/cliente/Requisicao.java
@@ -0,0 +1,24 @@
+package cliente;
+
+public class Requisicao {
+
+    private final int servico;
+    private final String corpo;
+
+    public Requisicao(int servico, String corpo) {
+        this.servico = servico;
+        this.corpo = corpo;
+    }
+
+    public String encode() {
+        return this.servico + "&&&" + this.corpo;
+    }
+
+    public int getServico() {
+        return servico;
+    }
+
+    public String getCorpo() {
+        return corpo;
+    }
+}
